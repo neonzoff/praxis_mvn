a54d0432fc1a948ea776c818f5e18beaa56b815c
neonzoff <neonzoff@gmail.com>
2020-08-05 12:45:18 +0300

    added some tests

--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SimpleCalculatorTest {
@@ -43,4 +44,49 @@ public class SimpleCalculatorTest {
         assertEquals(-20L,test);
     }
 
+    @Test
+    public void testSubInt() {
+        SimpleCalculator<Integer> simpleCalculator = new SimpleCalculator<>();
+        int test = simpleCalculator.sub(15,5);
+        assertEquals(10,test);
+    }
+
+    @Test
+    public void testSubDouble() {
+        SimpleCalculator<Double> simpleCalculator = new SimpleCalculator<>();
+        double delta = 0.0000001;
+        double test = simpleCalculator.sub(5.14d,.14d);
+        assertEquals(5d,test,delta);
+    }
+
+    @Test
+    public void testSubFloat() {
+        SimpleCalculator<Float> simpleCalculator = new SimpleCalculator<>();
+        float delta = 0.00001f;
+        float test = simpleCalculator.sub(4.14f,0.14f);
+        assertEquals(4f,test,delta);
+    }
+
+    //test mul
+    @Test
+    public void testMulLong() {
+        SimpleCalculator<Long> simpleCalculator = new SimpleCalculator<>();
+        long test = simpleCalculator.mul(5L,7L);
+        assertEquals(35L,test);
+    }
+
+    @Test
+    public void testMulInt() {
+        SimpleCalculator<Integer> simpleCalculator = new SimpleCalculator<>();
+        int test = simpleCalculator.mul(7,8);
+        assertEquals(56,test);
+    }
+
+    @Test
+    public void testMulDouble() {
+        SimpleCalculator<Double> simpleCalculator = new SimpleCalculator<>();
+        double delta = 0.000000001;
+        double test = simpleCalculator.mul(0.5d,10d);
+        assertEquals(5d,test,delta);
+    }
 }
