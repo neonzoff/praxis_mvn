3ea1ac4252d5c7dd96f996a0722ddb399c952018
neonzoff <neonzoff@gmail.com>
2020-08-05 12:24:33 +0300

    Create calculator

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -1,3 +1,76 @@
-public class Main {
+public class SimpleCalculator<T extends Number> {
 
+    private boolean checkIntegerTypes(T first,T second) {
+        return first instanceof Integer && second instanceof Integer;
+    }
+
+    private boolean checkLongTypes(T first,T second) {
+        return first instanceof Long && second instanceof Long;
+    }
+
+    private boolean checkDoubleTypes(T first,T second) {
+        return first instanceof Double && second instanceof Double;
+    }
+
+    private boolean checkFloatTypes(T first,T second) {
+        return first instanceof Float && second instanceof Float;
+    }
+
+    public T sum(T first, T second) {
+        if (checkIntegerTypes(first,second)) {
+            return (T) Integer.valueOf(first.intValue() + second.intValue());
+        } else if (checkLongTypes(first,second)) {
+            return (T) Long.valueOf(first.longValue() + second.longValue());
+        } else if (checkDoubleTypes(first,second)) {
+            return (T) Double.valueOf(first.doubleValue() + second.doubleValue());
+        } else if (checkFloatTypes(first,second)) {
+            return (T) Float.valueOf(first.floatValue() + second.floatValue());
+        } else {
+            return (T) Integer.valueOf(first.intValue() + second.intValue());
+        }
+    }
+
+    public T sub(T first, T second) {
+        if (checkIntegerTypes(first,second)) {
+            return (T) Integer.valueOf(first.intValue() - second.intValue());
+        } else if (checkLongTypes(first,second)) {
+            return (T) Long.valueOf(first.longValue() - second.longValue());
+        } else if (checkDoubleTypes(first,second)) {
+            return (T) Double.valueOf(first.doubleValue() - second.doubleValue());
+        }  else if (checkFloatTypes(first,second)) {
+            return (T) Float.valueOf(first.floatValue() - second.floatValue());
+        } else {
+            return (T) Integer.valueOf(first.intValue() - second.intValue());
+        }
+    }
+
+    public T mul (T first, T second) {
+        if (checkIntegerTypes(first,second)) {
+            return (T) Integer.valueOf(first.intValue() * second.intValue());
+        } else if (checkLongTypes(first,second)) {
+            return (T) Long.valueOf(first.longValue() * second.longValue());
+        } else if (checkDoubleTypes(first,second)) {
+            return (T) Double.valueOf(first.doubleValue() * second.doubleValue());
+        }  else if (checkFloatTypes(first,second)) {
+            return (T) Float.valueOf(first.floatValue() * second.floatValue());
+        } else {
+            return (T) Integer.valueOf(first.intValue() * second.intValue());
+        }
+    }
+
+    public T div (T first, T second) {
+        if (checkIntegerTypes(first,second)) {
+            return (T) Integer.valueOf(first.intValue() / second.intValue());
+        } else if (checkLongTypes(first,second)) {
+            return (T) Long.valueOf(first.longValue() / second.longValue());
+        } else if (checkDoubleTypes(first,second)) {
+            return (T) Double.valueOf(first.doubleValue() / second.doubleValue());
+        }  else if (checkFloatTypes(first,second)) {
+            return (T) Float.valueOf(first.floatValue() / second.floatValue());
+        } else {
+            return (T) Integer.valueOf(first.intValue() / second.intValue());
+        }
+    }
 }
+
+
--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class SimpleCalculatorTest {
+
+    //SumTestes
+    @Test
+    public void testSumLong() {
+        SimpleCalculator<Long> simpleCalculator = new SimpleCalculator<>();
+        long test = simpleCalculator.sum(100L,322L);
+        assertEquals(422L,test);
+    }
+
+    @Test
+    public void testSumInt() {
+        SimpleCalculator<Integer> simpleCalculator = new SimpleCalculator<>();
+        int test = simpleCalculator.sum(10,5);
+        assertEquals(15,test);
+    }
+
+    @Test
+    public void testSumDouble() {
+        SimpleCalculator<Double> simpleCalculator = new SimpleCalculator<>();
+        double delta = .00000001d;
+        double test = simpleCalculator.sum(3.14d,1.86d);
+        assertEquals(5d,test,delta);
+    }
+
+    @Test
+    public void testSumFloat() {
+        SimpleCalculator<Float> simpleCalculator = new SimpleCalculator<>();
+        float delta = .000001f;
+        float test = simpleCalculator.sum(5.17f,4.83f);
+        assertEquals(10f,test,delta);
+    }
+
+
+    //test sub
+    @Test
+    public void testSubLong() {
+        SimpleCalculator<Long> simpleCalculator = new SimpleCalculator<>();
+        long test = simpleCalculator.sub(-10L, 10L);
+        assertEquals(-20L,test);
+    }
+
 }
